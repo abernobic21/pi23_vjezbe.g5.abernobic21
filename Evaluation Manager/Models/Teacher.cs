e0b3de22b66939f2ca88917f417d9240f8ce7519
abernobic21 <abernobic21@student.foi.hr>
2023-05-23 15:58:58 +0200

    Implementiran INSERT/UPDATE evaluacije

--- Evaluation Manager/Models/Teacher.cs
+++ Evaluation Manager/Models/Teacher.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using Evaluation_Manager.Repozitories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Evaluation_Manager.Models {
-    public class Teacher : Person{
+    public class Teacher : Person
+    {
         public string Username { get; set; }
 
         public string Password { get; set; }
+
+        public void PerformEvaluation(Student student, Activity activity, int points)
+        {
+            var evaluation = EvaluationRepository.GetEvaluation(student, activity);
+            if (evaluation == null)
+            {
+                EvaluationRepository.InsertEvaluation(student, activity, this, points);
+            }
+            else
+            {
+                EvaluationRepository.UpdateEvaluation(evaluation, this, points);
+            }
+        }
     }
 }
+
